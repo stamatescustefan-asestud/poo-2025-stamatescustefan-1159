543f04990f4fcc7ff1f5229ed182e1fa3b0db23e
stamatescustefan-asestud <stamatescustefan24@stud.ase.ro>
2025-11-15 01:24:51 +0200

    Adaugat clase

--- Servicii de Distributie si Stocare/Servicii de Distributie si Stocare.cpp
+++ Servicii de Distributie si Stocare/Servicii de Distributie si Stocare.cpp
@@ -1,20 +1,34 @@
-// Servicii de Distributie si Stocare.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+using namespace std;
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+class Colet {
+public:
+	int greutate;
+	string destinatar;
+	string adresaDestinatar;
+	const int awb;
+	static int numarTotalColete;
+	char* descriereContinut;
+};
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+class Depozit {
+public:
+	string oras;
+	int capacitateMaximaColete;
+	int numarZoneSortate;
+	const int idDepozit;
+	static string numeCurier;
+	int* angajatiZona;
+};
+
+class Comanda {
+public:
+	string dataPlasare;
+	string status;
+	int valoareRamburs;
+	const int idComanda;
+	static int taxaTransport;
+	int* istoricDepozite;
+	int numarDepoziteIstoric;
+};
